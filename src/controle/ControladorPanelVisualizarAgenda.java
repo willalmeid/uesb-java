88abadfe08870c91300a4c311ed586e0525660b7
William Souza Almeida <141864990+willalmeid@users.noreply.github.com>
2024-10-30 15:50:02 -0300

    adicionado visualizar agenda

--- src/controle/ControladorPanelVisualizarAgenda.java
+++ src/controle/ControladorPanelVisualizarAgenda.java
@@ -1,13 +1,24 @@
 package controle;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import visual.PanelVisualizarAgenda;
 
-public class ControladorPanelVisualizarAgenda {
+public class ControladorPanelVisualizarAgenda implements ActionListener {
 
 	PanelVisualizarAgenda panelVisualizarAgenda;
 	
 	public ControladorPanelVisualizarAgenda(PanelVisualizarAgenda panelVisualizarAgenda) {
 		this.panelVisualizarAgenda = panelVisualizarAgenda;
+		addEventos();
+	}
+	
+	public void addEventos() {
+		
 	}
 
+	public void actionPerformed(ActionEvent e) {
+		
+	}
 }
--- src/visual/PanelVisualizarAgenda.java
+++ src/visual/PanelVisualizarAgenda.java
@@ -1,11 +1,43 @@
 package visual;
 
+import java.awt.Color;
+
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import java.awt.Font;
 
 public class PanelVisualizarAgenda extends JPanel {
 
+	private JLabel labelTitlePanel;
+	private JPanel panelPrincipal;
+	
 	public PanelVisualizarAgenda() {
-		// TODO Auto-generated constructor stub
+		setBackground(new Color(255, 128, 64)); // Apenas um teste
+		setSize(1280, 750);
+		setLayout(null);
+		add(getLabelTitlePanel());
+		add(getPanelPrincipal());
+	}
+	
+	public JLabel getLabelTitlePanel() {
+		if(labelTitlePanel == null) {
+			labelTitlePanel = new JLabel();
+			labelTitlePanel.setFont(new Font("Tahoma", Font.PLAIN, 36));
+			labelTitlePanel.setText("Visualizar Agenda");
+			labelTitlePanel.setBounds(497, 30, 285, 76);
+		}
+		return labelTitlePanel;
+	}
+	
+	public JPanel getPanelPrincipal() {
+		if(panelPrincipal == null) {
+			panelPrincipal = new JPanel();
+			panelPrincipal.setBackground(new Color(255, 128, 0));
+			panelPrincipal.setLocation(207, 136);
+			panelPrincipal.setSize(500, 400);
+			panelPrincipal.setBounds(30, 135, 1220, 490);
+		}
+		return panelPrincipal;
 	}
 
 }
