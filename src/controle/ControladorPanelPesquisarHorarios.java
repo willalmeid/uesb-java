24e7929c8b1a586010baeed133016125d1c5fcde
William Souza Almeida <141864990+willalmeid@users.noreply.github.com>
2024-10-30 15:57:23 -0300

    adicionado pesquisar horários

--- src/controle/ControladorPanelPesquisarHorarios.java
+++ src/controle/ControladorPanelPesquisarHorarios.java
@@ -1,13 +1,25 @@
 package controle;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import visual.PanelPesquisarHorarios;
 
-public class ControladorPanelPesquisarHorarios {
+public class ControladorPanelPesquisarHorarios implements ActionListener {
 
 	PanelPesquisarHorarios panelPesquisarHorarios;
 	
 	public ControladorPanelPesquisarHorarios(PanelPesquisarHorarios panelPesquisarHorarios) {
 		this.panelPesquisarHorarios = panelPesquisarHorarios;
+		addEventos();
+	}
+	
+	public void addEventos() {
+		
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		
 	}
 
 }
