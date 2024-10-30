6908b66d31611e4a836fd6fb374ae0dde6ac644c
William Souza Almeida <141864990+willalmeid@users.noreply.github.com>
2024-10-30 16:03:02 -0300

    adicionado relatório de consulta

--- src/controle/ControladorPanelRelatoriosDeConsulta.java
+++ src/controle/ControladorPanelRelatoriosDeConsulta.java
@@ -1,13 +1,25 @@
 package controle;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import visual.PanelRelatoriosDeConsulta;
 
-public class ControladorPanelRelatoriosDeConsulta {
+public class ControladorPanelRelatoriosDeConsulta implements ActionListener {
 
 	PanelRelatoriosDeConsulta panelRelatoriosDeConsulta;
 	
 	public ControladorPanelRelatoriosDeConsulta(PanelRelatoriosDeConsulta panelRelatoriosDeConsulta) {
 		this.panelRelatoriosDeConsulta = panelRelatoriosDeConsulta;
+		addEventos();
+	}
+	
+	public void addEventos() {
+		
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		
 	}
 
 }
--- src/visual/PanelRelatoriosDeConsulta.java
+++ src/visual/PanelRelatoriosDeConsulta.java
@@ -1,11 +1,43 @@
 package visual;
 
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class PanelRelatoriosDeConsulta extends JPanel {
 
+	private JLabel labelTitlePanel;
+	private JPanel panelPrincipal;
+	
 	public PanelRelatoriosDeConsulta() {
-		// TODO Auto-generated constructor stub
+		setBackground(new Color(0, 0, 255)); // Apenas um teste
+		setSize(1280, 750);
+		setLayout(null);
+		add(getLabelTitlePanel());
+		add(getPanelPrincipal());
+	}
+	
+	public JLabel getLabelTitlePanel() {
+		if(labelTitlePanel == null) {
+			labelTitlePanel = new JLabel();
+			labelTitlePanel.setFont(new Font("Tahoma", Font.PLAIN, 36));
+			labelTitlePanel.setText("Relatório de Consulta");
+			labelTitlePanel.setBounds(467, 30, 345, 76);
+		}
+		return labelTitlePanel;
+	}
+	
+	public JPanel getPanelPrincipal() {
+		if(panelPrincipal == null) {
+			panelPrincipal = new JPanel();
+			panelPrincipal.setBackground(new Color(0, 0, 160));
+			panelPrincipal.setLocation(207, 136);
+			panelPrincipal.setSize(500, 400);
+			panelPrincipal.setBounds(30, 135, 1220, 490);
+		}
+		return panelPrincipal;
 	}
 
 }
