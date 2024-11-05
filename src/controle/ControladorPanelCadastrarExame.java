491b2a9cdbf7d7bfa857af7f0034039f60e2f2c6
Ian Alves Bezerra <IanBezerra.IA@gmail.com>
2024-11-05 18:30:10 -0300

    Cadastrar exame

--- src/controle/ControladorPanelCadastrarExame.java
+++ src/controle/ControladorPanelCadastrarExame.java
@@ -2,15 +2,23 @@ package controle;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
+import javax.swing.JOptionPane;
+
+import modelo.Exame;
+import modelo.Material;
+import modelo.Medico;
 import visual.PanelCadastrarExame;
 
 public class ControladorPanelCadastrarExame implements ActionListener{
 
 	PanelCadastrarExame panelCadastrarExame;
+	ArrayList<Exame> examesCadastrados;
 	
 	public ControladorPanelCadastrarExame(PanelCadastrarExame panelCadastrarExame) {
 		this.panelCadastrarExame = panelCadastrarExame;
+		examesCadastrados = new ArrayList<Exame>();
 		addEventos();
 	}
 	
@@ -31,7 +39,24 @@ public class ControladorPanelCadastrarExame implements ActionListener{
 
 
 	public void cadastrarExame() {
-		
+		try {
+			String nome = panelCadastrarExame.getTextFieldNomeExame().getText();
+			String descricao = panelCadastrarExame.getTextAreaDescricao().getText();
+			Material materiaisUtilizados = new Material();
+			materiaisUtilizados.setNomeDoMaterial(panelCadastrarExame.getTextAreaMateriaisUtilizados().getText());
+			float valorParticular = Float.parseFloat(panelCadastrarExame.getTextFieldValorParticular().getText());
+			
+	    	Medico medico = (Medico)panelCadastrarExame.getComboBoxMedico().getSelectedItem();
+	    	String tipoExame = (String)panelCadastrarExame.getComboBoxTipoExame().getSelectedItem();
+	    	
+	    	Exame e = new Exame(nome, descricao, valorParticular, materiaisUtilizados, medico, tipoExame);
+	    	
+	    	examesCadastrados.add(e);
+	    	JOptionPane.showMessageDialog(panelCadastrarExame, "Exame "+nome+" cadastrado com sucesso", "Sucesso!", JOptionPane.INFORMATION_MESSAGE);
+			
+		} catch(Exception e){
+			JOptionPane.showMessageDialog(panelCadastrarExame, "Erro! Digite números válidos", "Erro!", JOptionPane.WARNING_MESSAGE);
+		}
 	}
 	
 	public void limparPanel() {
