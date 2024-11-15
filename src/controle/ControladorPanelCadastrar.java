20c08cae96fb040ef426efa35b4ea48572d9fb3b
William Souza Almeida <141864990+willalmeid@users.noreply.github.com>
2024-11-15 17:24:33 -0300

    adicionando controlador do panelcadastrar

---
+++ src/controle/ControladorPanelCadastrar.java
@@ -0,0 +1,35 @@
+package controle;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import visual.Dialog;
+import visual.PanelCadastrar;
+import visual.PanelCadastrarMedico;
+import visual.PanelCadastrarPaciente;
+
+public class ControladorPanelCadastrar implements ActionListener {
+
+	PanelCadastrar panelCadastrar;
+	
+	public ControladorPanelCadastrar(PanelCadastrar panelCadastrar) {
+		this.panelCadastrar = panelCadastrar;
+		addEventos();
+	}
+	
+	public void addEventos() {
+		panelCadastrar.getButtonCadastrarPaciente().addActionListener(this);
+		panelCadastrar.getButtonCadastrarMedico().addActionListener(this);
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == panelCadastrar.getButtonCadastrarPaciente()) {
+			PanelCadastrarPaciente panelCadastrarPaciente = new PanelCadastrarPaciente();
+			Dialog dialog = new Dialog(panelCadastrarPaciente);
+		} else if(e.getSource() == panelCadastrar.getButtonCadastrarMedico()) {
+			PanelCadastrarMedico panelCadastrarMedico = new PanelCadastrarMedico();
+			Dialog dialog = new Dialog(panelCadastrarMedico);
+		}
+	}
+
+}
