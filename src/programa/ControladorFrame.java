fc5b9de6cb2a956cf7aaed2e78780360d7e45d30
William Souza Almeida <141864990+willalmeid@users.noreply.github.com>
2024-11-15 17:26:04 -0300

    configuração das ações iniciais

--- src/programa/ControladorFrame.java
+++ src/programa/ControladorFrame.java
@@ -1,39 +1,67 @@
 package programa;
 
+import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import controle.ControladorPanelCadastrar;
+import styles.ButtonNavegacao;
 
 import visual.Dialog;
 import visual.Frame;
+import visual.PanelHome;
+import visual.PanelMain;
 import visual.PanelCadastrar;
+import visual.PanelCadastrarMedico;
 import visual.PanelCadastrarPaciente;
 
 public class ControladorFrame implements ActionListener{
 
 	Frame frame;
+	PanelHome panelHome;
+	PanelMain panelMain;
 	
 	public ControladorFrame(Frame frame) {
 		this.frame = frame;
+		
+		if (frame.getContentPane() instanceof PanelHome) {
+            this.panelHome = (PanelHome) frame.getContentPane();
+
+            // Obtém o PanelMain através do JSplitPane
+            if (panelHome.getSplitPane().getRightComponent() instanceof PanelMain) {
+                this.panelMain = (PanelMain) panelHome.getSplitPane().getRightComponent();
+
+                // Adiciona ActionListener aos botões
+                ButtonNavegacao buttonCadastrar = panelMain.getButtonCadastar();
+                ButtonNavegacao buttonAgendar = panelMain.getButtonAgendar();
+                ButtonNavegacao buttonRelatorios = panelMain.getButtonRelatorios();
+
+                buttonCadastrar.addActionListener(this);
+                buttonAgendar.addActionListener(this);
+                buttonRelatorios.addActionListener(this);
+            }
+        }
 	}
 	
 	public void actionPerformed(ActionEvent e) {
-		
+		if (e.getSource() == panelMain.getButtonCadastar()) {
+			PanelCadastrar panelCadastrar = new PanelCadastrar();
+			ControladorPanelCadastrar controladorCadastrar = new ControladorPanelCadastrar(panelCadastrar);
+			frame.setContentPane(panelCadastrar);
+			
+        } else if (e.getSource() == panelMain.getButtonAgendar()) {
+            System.out.println("Botão 'Agendar' clicado!");
+        } else if (e.getSource() == panelMain.getButtonRelatorios()) {
+            System.out.println("Botão 'Relatórios' clicado!");
+        }
 	}
 	
 	public static void main(String[] args) {
 		Frame frame = new Frame();
-		// Para testar a pagina Home
-//		PanelHome panelHome = new PanelHome();
-//		frame.setContentPane(panelHome);
-		
-		// Para testar a página Cadastrar
-		PanelCadastrar panelCadastrar = new PanelCadastrar();
-		frame.setContentPane(panelCadastrar);
-		
-		// Para testar a página Cadastrar paciente
-		PanelCadastrarPaciente paciente = new PanelCadastrarPaciente();
-		new Dialog(paciente, frame);
-
+		PanelHome panelHome = new PanelHome();
+		frame.setContentPane(panelHome);
 		new ControladorFrame(frame);
 	}
 }
