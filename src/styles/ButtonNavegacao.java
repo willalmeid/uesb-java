4eeafb93d4cea501848c1c04a59f76ecf2f0a762
William Souza Almeida <141864990+willalmeid@users.noreply.github.com>
2024-11-17 18:16:13 -0300

    criação de novo estilo

--- src/styles/ButtonNavegacao.java
+++ src/styles/ButtonNavegacao.java
@@ -2,48 +2,20 @@ package styles;
 
 import java.awt.Component;
 import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.RenderingHints;
 
 import javax.swing.JButton;
-import javax.swing.SwingConstants;
 
 public class ButtonNavegacao extends JButton {
-    public ButtonNavegacao(String text) {
-    	setText(text);
-        setFont(Thema.FONT_BUTTON_NAVEGACAO);
-        setBackground(Thema.TRANSPARENT);
-        setForeground(Thema.PRINCIPAL);
-        setPreferredSize(new Dimension(300, 500));
-        
-        setAlignmentX(Component.CENTER_ALIGNMENT);
-        setVerticalTextPosition(SwingConstants.BOTTOM); // Texto embaixo
-        setHorizontalTextPosition(SwingConstants.CENTER); // Centralizado
-        
-        setBorderPainted(false);          // Não desenhar borda padrão
-        setFocusPainted(false);           // Não desenhar borda de foco
-        setOpaque(true);                  // Garantir que o botão seja opaco
-        setContentAreaFilled(true);       // Habilitar a área de conteúdo preenchida
-    }
 
-    public void paintBorder(Graphics g) {
-        // Não faz nada, pois queremos controlar a borda manualmente
-    }
-
-    protected void paintComponent(Graphics g) {
-        Graphics2D g2 = (Graphics2D) g.create();
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-
-        // Preenchendo o fundo com cor arredondada
-        g2.setColor(Thema.WHITE);
-        g2.fillRoundRect(0, 0, getWidth(), getHeight(), 10, 10); // 20, 20 define os cantos arredondados
-
-        // Desenhando a borda arredondada
-        g2.setColor(Thema.AUXILIAR);
-        g2.drawRoundRect(0, 0, getWidth() - 1, getHeight() - 1, 10, 10); // Borda arredondada
-
-        g2.dispose();
-        super.paintComponent(g); // Continua desenhando o conteúdo do botão
-    }
+	public ButtonNavegacao() {
+		setBackground(Thema.TRANSPARENT);
+		setPreferredSize(new Dimension(32, 32));
+		
+		setAlignmentX(Component.CENTER_ALIGNMENT);
+		
+		setBorderPainted(false);
+		setFocusPainted(false);
+		setOpaque(false);
+		setContentAreaFilled(false);
+	}
 }
