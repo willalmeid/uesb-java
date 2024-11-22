28415e9bb4eb83e55a0a5d8d02d8a7bb90179f6c
William Souza Almeida <141864990+willalmeid@users.noreply.github.com>
2024-11-21 21:45:19 -0300

    rename icons + adicionando icons

--- src/styles/Icons.java
+++ src/styles/Icons.java
@@ -5,22 +5,22 @@ public class Icons {
 //	Caminho dos icons
 //	/assets/icons/*.png
 	
-	public static final String CADASTRAR = "/assets/icons/adicionar.png";
-	public static final String AGENDAR = "/assets/icons/editar.png";
-	public static final String RELATORIO = "/assets/icons/documento.png";
+	public static final String CADASTRAR = "/assets/icons/icon-adicionar.png";
+	public static final String AGENDAR = "/assets/icons/icon-editar.png";
+	public static final String RELATORIO = "/assets/icons/icon-documentos.png";
 
-	public static final String CADASTRAR_PACIENTE = "/assets/icons/paciente.png";
-	public static final String CADASTRAR_MEDICO = "/assets/icons/medico.png";
-	public static final String CADASTRAR_EXAME = "/assets/icons/exame.png";
-	public static final String CADASTRAR_CONSULTA = "/assets/icons/consulta.png";
-	public static final String CADASTRAR_MATERIAL = "/assets/icons/material.png";
+	public static final String CADASTRAR_PACIENTE = "/assets/icons/icon-paciente.png";
+	public static final String CADASTRAR_MEDICO = "/assets/icons/icon-medico.png";
+	public static final String CADASTRAR_EXAME = "/assets/icons/icon-exame.png";
+	public static final String CADASTRAR_CONSULTA = "/assets/icons/icon-consulta.png";
+	public static final String CADASTRAR_MATERIAL = "/assets/icons/icon-material.png";
 	
-	public static final String AGENDAR_CONSULTA = "/assets/icons/consulta.png";
-	public static final String AGENDAR_EXAME = "/assets/icons/exame.png";
-	public static final String VISUALIZAR_AGENDA = "/assets/icons/calendario.png";
-	public static final String PESQUISAR_HORARIOS = "/assets/icons/relogio-calendario.png";
-	public static final String CANCELAR_E_REAGENDAR = "/assets/icons/cancelar-e-reagendar.png";
+	public static final String AGENDAR_CONSULTA = "/assets/icons/icon-agendar-consulta.png";
+	public static final String AGENDAR_EXAME = "/assets/icons/icon-agendar-exame.png";
+	public static final String VISUALIZAR_AGENDA = "/assets/icons/icon-calendario.png";
+	public static final String PESQUISAR_HORARIOS = "/assets/icons/icon-horarios.png";
+	public static final String CANCELAR_E_REAGENDAR = "/assets/icons/icon-alterar-consulta.png";
 
-	public static final String BUTTON_HOME = "/assets/icons/home.png";
-	public static final String BUTTON_BACK = "/assets/icons/voltar.png";
+	public static final String BUTTON_HOME = "/assets/icons/icon-home.png";
+	public static final String BUTTON_BACK = "/assets/icons/icon-voltar.png";
 }
