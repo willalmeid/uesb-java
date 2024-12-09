dbb2ee7cb78a8dfc9c40b0e03e0c110038e25959
William Souza Almeida <141864990+willalmeid@users.noreply.github.com>
2024-12-09 20:35:11 -0300

    copiando os códigos da animação da bola

--- src/controle/ControladorFrame.java
+++ src/controle/ControladorFrame.java
@@ -2,12 +2,21 @@ package controle;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Timer;
 
 import visual.Frame;
 
 public class ControladorFrame implements ActionListener {
 
 	private Frame frame;
+	Timer timer;
+    
+    int posicaoY = 60;
+    double velocidade = 0;
+    double aceleracao = 10;
+    double fatorRebote = 0.8;
+    long tempoIntervalo = 10;
+    int limiteInferior = 715;
 	
 	public ControladorFrame(Frame frame) {
 		this.frame = frame;
@@ -20,10 +29,55 @@ public class ControladorFrame implements ActionListener {
 	
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource() == frame.getButtonStart()) {
+			iniciarSimulacao();
 			System.out.println("TESTE");
 		}
 		
 	}
+	
+	public void iniciarSimulacao() {
+		frame.getButtonStart().setEnabled(false);
+
+//    	posicaoY = 60;
+//        velocidade = 0;
+//        
+//        if (timer != null) {
+//            timer.cancel(); // Cancela qualquer timer anterior para evitar conflitos
+//        }
+//        
+//        timer = new Timer();
+//        timer.scheduleAtFixedRate(new TimerTask() {
+//            public void run() {
+                atualizarPosicao(); // Chama a função de atualização da posição
+//            }
+//        }, 0, tempoIntervalo); // Intervalo definido para cada 10ms
+	}
+	
+	public void atualizarPosicao() {
+		
+//		posicaoY += (int) velocidade;
+//        velocidade += (aceleracao * tempoIntervalo) / 1000;
+//
+//        if (posicaoY >= limiteInferior) {
+//            posicaoY = limiteInferior;
+//            velocidade = -velocidade * fatorRebote;
+//            
+//            if (Math.abs(velocidade) < 0.1) { // Ajuste o valor de acordo com a precisão desejada
+                finalizarSimulacao();
+//            }
+//        }
+//
+//        frame.getLabelBola().setBounds(150, posicaoY, 40, 40); // Atualiza a posição no frame
+		
+	}
+	
+	private void finalizarSimulacao() {
+//        if (timer != null) {
+//            timer.cancel();
+//            timer = null;
+//        }
+        frame.getButtonStart().setEnabled(true);
+    }
 	
 	public static void main(String[] args) {
 		new ControladorFrame(new Frame());
