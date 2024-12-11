aa59fc54df3eccb59ad51b47e4a237fb00f80bd0
William Souza Almeida <141864990+willalmeid@users.noreply.github.com>
2024-12-11 17:54:05 -0300

    adição dos panels relatorios

---
+++ src/visual/PanelRelatoriosFinanceiros.java
@@ -0,0 +1,43 @@
+package visual;
+
+import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
+
+import styles.Thema;
+import styles.TitlePanel;
+import javax.swing.JLabel;
+
+public class PanelRelatoriosFinanceiros extends JPanel {
+
+	private TitlePanel labelTitlePanel;
+	private JPanel panelPrincipal;
+	private JLabel lblNewLabel;
+	
+	public PanelRelatoriosFinanceiros() {
+		setSize(830, 600);
+		setLayout(null);
+		setBackground(Thema.PRINCIPAL);
+		add(getPanelMain());
+		add(getLabelTitlePanel());
+	}
+
+	public JPanel getPanelMain() {
+		if(panelPrincipal == null) {
+			panelPrincipal = new JPanel();
+			panelPrincipal.setLocation(20, 80);
+			panelPrincipal.setSize(760, 440);
+			panelPrincipal.setBorder(new LineBorder(null));
+			panelPrincipal.setBackground(Thema.BACKGROUND);
+			panelPrincipal.setLayout(null);
+		}
+		return panelPrincipal;
+	}
+	
+	public TitlePanel getLabelTitlePanel() {
+		if(labelTitlePanel == null) {
+			labelTitlePanel = new TitlePanel("Relatório Financeiro");
+			labelTitlePanel.setBounds(209, 22, 412, 39);
+		}
+		return labelTitlePanel;
+	}
+}
