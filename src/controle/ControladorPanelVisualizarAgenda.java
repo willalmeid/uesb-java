7a0661ed8fc6f40802d72f06be11ac7a89b00ad0
William Souza Almeida <141864990+willalmeid@users.noreply.github.com>
2024-12-11 16:52:38 -0300

    Create ControladorPanelVisualizarAgenda.java

---
+++ src/controle/ControladorPanelVisualizarAgenda.java
@@ -0,0 +1,19 @@
+package controle;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import visual.PanelAgendarVisualizarAgenda;
+
+public class ControladorPanelVisualizarAgenda implements ActionListener {
+
+	PanelAgendarVisualizarAgenda panelAgendarVisualizarAgenda;
+	
+	public ControladorPanelVisualizarAgenda(PanelAgendarVisualizarAgenda panelAgendarVisualizarAgenda) {
+		this.panelAgendarVisualizarAgenda = panelAgendarVisualizarAgenda;
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+}
