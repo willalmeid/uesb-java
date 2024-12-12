7a7234becca9d4bb4051ac594cb90ed535538f67
William Souza Almeida <141864990+willalmeid@users.noreply.github.com>
2024-12-12 14:51:46 -0300

    teste de troca de icon do frame

--- src/visual/Frame.java
+++ src/visual/Frame.java
@@ -3,11 +3,13 @@ package visual;
 import javax.swing.JFrame;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
+import java.awt.Toolkit;
 
 public class Frame extends JFrame {
 
 	public Frame() {
 		super();
+		setIconImage(Toolkit.getDefaultToolkit().getImage(Frame.class.getResource("/assets/icons/icon-adicionar.png")));
 		this.setVisible(true);
 		this.setSize(1280, 720);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
