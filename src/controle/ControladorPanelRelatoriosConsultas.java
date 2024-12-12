72836001ef2c4f12ac4f757a728953902a5957d1
William Souza Almeida <141864990+willalmeid@users.noreply.github.com>
2024-12-12 18:48:24 -0300

    Create ControladorPanelRelatoriosConsultas.java

---
+++ src/controle/ControladorPanelRelatoriosConsultas.java
@@ -0,0 +1,19 @@
+package controle;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import visual.PanelRelatoriosConsultas;
+
+public class ControladorPanelRelatoriosConsultas implements ActionListener {
+
+	PanelRelatoriosConsultas panelRelatoriosConsultas;
+	
+	public ControladorPanelRelatoriosConsultas(PanelRelatoriosConsultas panelRelatoriosConsultas) {
+		this.panelRelatoriosConsultas = panelRelatoriosConsultas;
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+}
